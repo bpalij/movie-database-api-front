f63b4279e301177311a4355fe2c509d39b198580
bpalij <bpalij@gmail.com>
2019-05-13 14:15:37 +0300

    Updates

---
+++ precache-manifest.0995b92154dba9eefab8327b0a2dce8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf96a9ce9beaa0833ffa57e5b1763ff8",
+    "url": "/movie-database-api-front/index.html"
+  },
+  {
+    "revision": "f66e60c68cffd2086442",
+    "url": "/movie-database-api-front/static/css/2.5049d053.chunk.css"
+  },
+  {
+    "revision": "9e13ca50ce28b9eb1a16",
+    "url": "/movie-database-api-front/static/css/main.b9e6a72b.chunk.css"
+  },
+  {
+    "revision": "f66e60c68cffd2086442",
+    "url": "/movie-database-api-front/static/js/2.0e30c2fb.chunk.js"
+  },
+  {
+    "revision": "9e13ca50ce28b9eb1a16",
+    "url": "/movie-database-api-front/static/js/main.27aa800e.chunk.js"
+  },
+  {
+    "revision": "1cae512039e2d65064f5",
+    "url": "/movie-database-api-front/static/js/runtime~main.9e81866a.js"
+  }
+]);
